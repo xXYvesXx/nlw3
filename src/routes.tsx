66f6124c63559d5f984b1d8cc6ca011196989e1b
xXYvesXx <yvesh20@gmail.com>
2020-10-18 13:41:22 -0300

    criação da rota orphanages map

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Landing from './pages/Landing'
-import Orphanages from './pages/OrphanagesMap'
+import OrphanagesMap from './pages/OrphanagesMap'
 
 function Routes() {
   return (
     <BrowserRouter>
       <Route path="/" component={Landing} />
-      <Route path="/app" component={Orphanages} />
+      <Route path="/app" component={OrphanagesMap} />
     </BrowserRouter>
   )
 }
